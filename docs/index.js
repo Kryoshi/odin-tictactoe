7c825c74b4ad526a9d732fe8f39863d96591bb92
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-05-02 14:09:49 +0530

    Complete console game

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,17 @@
+const AXES = {
+    row_top         : "top left, top center, top right",
+    row_middle      : "middle left, middle center, middle right",
+    row_bottom      : "bottom left, bottom center, bottom right",
+
+    column_left     : "top left, middle left, bottom left",
+    column_center   : "top center, middle center, bottom center",
+    column_right    : "top right, middle right, bottom right",
+
+    diagonal_left   : "top left, middle center, bottom right",
+    diagonal_right  : "top right, middle center, bottom left"
+};
+
 function createBoard() {
-    const rows = 3;
-    const columns = 3;
     const board = {
         "top" : createRow(),
         "middle" : createRow(),
@@ -10,6 +21,7 @@ function createBoard() {
     const placeMarker = (rowKey, columnKey, marker) => {
         if (!getMarker(rowKey, columnKey)) {
             board[rowKey][columnKey].setMarker(marker);
+            console.log(getMarker(rowKey, columnKey));
         };
     };
 
@@ -18,18 +30,18 @@ function createBoard() {
     const checkRow = (rowKey) => {
         switch (rowKey) {
             case "top" :
-                if (compareCells("top", "left", "top", "center", "top", "right")) {
-
+                if (compareCells(AXES.row_top)) {
+                    return AXES.row_top;
                 };
             break;
             case "middle" :
-                if (compareCells("middle", "left", "middle", "center", "middle", "right")) {
-
+                if (compareCells(AXES.row_middle)) {
+                    return AXES.row_middle;
                 };
             break;
             case "bottom" :
-                if (compareCells("bottom", "left", "bottom", "center", "bottom", "right")) {
-
+                if (compareCells(AXES.row_bottom)) {
+                    return AXES.row_bottom;
                 };
             break;
         }
@@ -38,18 +50,18 @@ function createBoard() {
     const checkColumn = (columnKey) => {
         switch (columnKey) {
             case "left" :
-                if (compareCells("top", "left", "middle", "left", "bottom", "left")) {
-
+                if (compareCells(AXES.column_left)) {
+                    return AXES.column_left;
                 };
             break;
             case "center" :
-                if (compareCells("top", "center", "middle", "center", "bottom", "center")) {
-
+                if (compareCells(AXES.column_center)) {
+                    return AXES.column_center;
                 };
             break;
             case "right" :
-                if (compareCells("top", "right", "middle", "right", "bottom", "right")) {
-
+                if (compareCells(AXES.column_right)) {
+                    return AXES.column_right;
                 };
             break;
         }
@@ -60,35 +72,34 @@ function createBoard() {
             "topleft" ||
             "middlecenter" ||
             "bottomright") {
-                if (compareCells("top", "left", "middle", "center", "bottom", "right")) {
-
+                if (compareCells(AXES.diagonal_left)) {
+                    return AXES.diagonal_left;
                 };
         } else if ((rowKey + columnKey) ===
             "topright" ||
             "middlecenter" ||
             "bottomleft") {
-                if (compareCells("top", "right", "middle", "center", "bottom", "left")) {
-
+                if (compareCells(AXES.diagonal_right)) {
+                    return AXES.diagonal_right;
                 };
         }
     };
 
-    function compareCells(firstCellRow, firstCellColumn,
-                        secondCellRow, secondCellColumn,
-                        thirdCellRow = false, thirdCellColumn = false) {
+    function compareCells(axis) {
+        const cells = axis.split(", ");
+        const firstCell = cells[0].split(" ");
+        const secondCell = cells[1].split(" ");
+        const thirdCell = cells[2].split(" ");
 
-        if (thirdCellRow && thirdCellColumn) {
-            return ((getMarker(firstCellRow, firstCellColumn)
-                    ===
-                    getMarker(secondCellRow, secondCellColumn))
-                    &&
-                    (getMarker(firstCellRow, firstCellColumn)
-                    ===
-                    getMarker(thirdCellRow, thirdCellColumn)));
-        }
-        else return (getMarker(firstCellRow, firstCellColumn)
-                    ===
-                    getMarker(secondCellRow, secondCellColumn));
+
+        return ((getMarker(firstCell[0], firstCell[1])
+                ===
+                getMarker(secondCell[0], secondCell[1]))
+                &&
+                (getMarker(firstCell[0], firstCell[1])
+                ===
+                getMarker(thirdCell[0], thirdCell[1])));
+
     };
 
     function createRow() {
@@ -104,6 +115,7 @@ function createBoard() {
     
         const setMarker = (_marker) => {
             marker = _marker;
+            print();
         };
     
         const getMarker = () => marker;
@@ -111,26 +123,32 @@ function createBoard() {
         return {setMarker, getMarker};
     }
 
-    const print = () => console.log(`${board["top"]["left"].getMarker()} ${board["top"]["center"].getMarker()} ${board["top"]["left"].getMarker()}\n` +
+    const print = () => console.log(`${board["top"]["left"].getMarker()} ${board["top"]["center"].getMarker()} ${board["top"]["right"].getMarker()}\n` +
                                     `${board["middle"]["left"].getMarker()} ${board["middle"]["center"].getMarker()} ${board["middle"]["right"].getMarker()}\n` +
                                     `${board["bottom"]["left"].getMarker()} ${board["bottom"]["center"].getMarker()} ${board["bottom"]["right"].getMarker()}`);
 
     return {placeMarker, getMarker, checkRow, checkColumn, checkDiagonal, print};
 }
 
-function createPlayer(marker = "") {
+function createPlayer(name = "", marker = "") {
+    const setName = (_name) => {
+        name = _name
+    };
+    const getName = () => name;
+
     const setMarker = (_marker) => {
         marker = _marker
     };
     const getMarker = () => marker;
 
-    return {setMarker, getMarker};
+    return {setMarker, getMarker, setName, getName};
 }
 
 const game = (function () {
     const board = createBoard();
-    const players = [createPlayer("X"), createPlayer("O")];
+    const players = [createPlayer("X","X"), createPlayer("O","O")];
     let activePlayer = players[coinFlip()];
+
     const lastMove = {row : 0, column: 0};
 
     const switchPlayerTurn = () => {
@@ -151,10 +169,20 @@ const game = (function () {
         if (!win) win = board.checkColumn(lastMove.column);
         if (!win) win = board.checkDiagonal(lastMove.row, lastMove.column);
         if (!win) return false;
-        else return {activePlayer, win};
+        else return {name : activePlayer.getName(), lastMove, win};
     }
+
+    return {switchPlayerTurn, getActivePlayer, playRound, checkWin};
 })();
 
 function coinFlip() {
     return Math.floor(Math.random() * 2);
 }
+
+let win = false;
+while (!win) {
+    game.switchPlayerTurn();
+    game.playRound(prompt(), prompt());
+    win = game.checkWin();
+}
+console.log(win);
